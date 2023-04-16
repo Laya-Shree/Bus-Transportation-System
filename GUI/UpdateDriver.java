cbe6c7662d7e6c8aa5949e50f54632f6d8885ab2
Laya Shree Elango <layashreeelango@gmail.com>
2023-04-16 11:45:56 +0400

    Update Driver Feature Complete

--- GUI/UpdateDriver.java
+++ GUI/UpdateDriver.java
@@ -30,7 +30,10 @@ public class UpdateDriver extends javax.swing.JFrame {
     private void initComponents() {
 
         JButton btnEdit = new JButton();
-        textfUpdateDriver = new javax.swing.JTextField();
+        JButton btnAdd = new JButton();
+        JButton btnDelete = new JButton();
+        textfname = new javax.swing.JTextField();
+        textfcontact = new javax.swing.JTextField();
         textfid = new javax.swing.JTextField();
         idlabel = new javax.swing.JLabel();
         namelabel = new javax.swing.JLabel();
@@ -50,15 +53,6 @@ public class UpdateDriver extends javax.swing.JFrame {
 
        
 
-        textfUpdateDriver.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                //textField1ActionPerformed(evt);
-            }
-        });
-
-        getContentPane().add(textfUpdateDriver);
-        textfUpdateDriver.setBounds(450, 290, 180, 30);
-
         textfid.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
                 //textField1ActionPerformed(evt);
@@ -66,7 +60,25 @@ public class UpdateDriver extends javax.swing.JFrame {
         });
 
         getContentPane().add(textfid);
-        textfid.setBounds(450, 380, 180, 30);
+        textfid.setBounds(450, 290, 180, 30);
+
+        textfname.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                //textField1ActionPerformed(evt);
+            }
+        });
+
+        getContentPane().add(textfname);
+        textfname.setBounds(450, 380, 180, 30);
+
+        textfcontact.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                //textField1ActionPerformed(evt);
+            }
+        });
+
+        getContentPane().add(textfcontact);
+        textfcontact.setBounds(450, 470, 180, 30);
 
 
         try{ 
@@ -81,7 +93,7 @@ public class UpdateDriver extends javax.swing.JFrame {
             String data[][] = new String[rows][];  
 
             ResultSet rs1=stmt.executeQuery("SELECT * FROM Driver");
-            // DefaultTableModel table = (DefaultTableModel) studentdetails.getModel();
+            // DefaultTableModel table = (DefaultTableModel) driverdetails.getModel();
             
             int j=0;
             while(rs1.next()){
@@ -93,8 +105,18 @@ public class UpdateDriver extends javax.swing.JFrame {
                 j++;
             }  
             String column[]={"Driver_ID","Driver_Name","Contact No."};
-            studentdetails = new JTable(data,column);
-            JScrollPane sp=new JScrollPane(studentdetails);
+            driverdetails = new JTable(data,column);
+
+            driverdetails.addMouseListener(new MouseAdapter(){
+                public void mouseClicked (MouseEvent evt){
+                        JTable target = (JTable)evt.getSource();
+                        int selectedrow = target.getSelectedRow();
+                        textfid.setText(driverdetails.getModel().getValueAt(selectedrow,0).toString());
+                        textfname.setText(driverdetails.getModel().getValueAt(selectedrow,1).toString());
+                        textfcontact.setText(driverdetails.getModel().getValueAt(selectedrow,2).toString());
+                }
+            });
+            JScrollPane sp=new JScrollPane(driverdetails);
             sp.setBounds(800,180,1000,600);
             getContentPane().add(sp);
             setVisible(false);
@@ -142,7 +164,30 @@ public class UpdateDriver extends javax.swing.JFrame {
         getContentPane().add(btnBack);
         btnBack.setBounds(10, 10, 90, 32);
 
-        //Edit Button
+        //Add Button
+        btnAdd.setBackground(new Color(112, 161, 180));
+        btnAdd.setForeground(Color.WHITE);
+        btnAdd.setUI(new StyledButtonUI());
+        btnAdd.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent evt) {
+                btnAdd.setBackground(new Color(92, 132, 147));
+            }
+            public void mouseExited(MouseEvent evt) {
+                btnAdd.setBackground(new Color(112, 161, 180));
+            }
+        });
+        btnAdd.setFont(new java.awt.Font("SansSerif", 0, 12)); 
+        btnAdd.setText("Add");
+        btnAdd.setForeground(Color.white);
+        btnAdd.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                btnAddActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnAdd);
+        btnAdd.setBounds(200, 540, 70, 32);
+
+        //Edit button
         btnEdit.setBackground(new Color(112, 161, 180));
         btnEdit.setForeground(Color.WHITE);
         btnEdit.setUI(new StyledButtonUI());
@@ -165,27 +210,47 @@ public class UpdateDriver extends javax.swing.JFrame {
         getContentPane().add(btnEdit);
         btnEdit.setBounds(380, 540, 70, 32);
 
-
-       
+        //Delete Button
+        btnDelete.setBackground(new Color(112, 161, 180));
+        btnDelete.setForeground(Color.WHITE);
+        btnDelete.setUI(new StyledButtonUI());
+        btnDelete.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent evt) {
+                btnDelete.setBackground(new Color(92, 132, 147));
+            }
+            public void mouseExited(MouseEvent evt) {
+                btnDelete.setBackground(new Color(112, 161, 180));
+            }
+        });
+        btnDelete.setFont(new java.awt.Font("SansSerif", 0, 12)); 
+        btnDelete.setText("Delete");
+        btnDelete.setForeground(Color.white);
+        btnDelete.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                btnDeleteActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnDelete);
+        btnDelete.setBounds(560, 540, 70, 32);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-    getContentPane().setLayout(layout);
-    layout.setHorizontalGroup(
-        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-        .addComponent(panelStatus, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        .addComponent(dp)
-    );
-    layout.setVerticalGroup(
-        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelStatus, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(dp)
-            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-            .addComponent(panelStatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-    );
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(dp)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelStatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
 
-        pack();
+            pack();
     }
-    
+
     private void btnBackActionPerformed(ActionEvent evt) {
         AdminForm ss = new AdminForm();
         ss.setVisible(true);
@@ -193,19 +258,45 @@ public class UpdateDriver extends javax.swing.JFrame {
         this.dispose();
         
     }
-    
 
-    private void btnEditActionPerformed(ActionEvent evt) {
-        int UpdateDriver = Integer.parseInt(textfUpdateDriver.getText());
-        String id= textfid.getText();
+    private void btnAddActionPerformed(ActionEvent evt) {
+        String driverID = textfid.getText();
+        String dname = textfname.getText();
+        int contactno = Integer.parseInt(textfcontact.getText());
+
         try{  
             Class.forName("com.mysql.cj.jdbc.Driver");  
             Connection con=DriverManager.getConnection(  
             "jdbc:mysql://localhost:3306/bus_system","root","*Laya2003*");  
              
-            PreparedStatement insert = con.prepareStatement("UPDATE Driver set UpdateDriver = ? WHERE ID = ?;");
-            insert.setString(2,id);
-            insert.setInt(1,UpdateDriver);
+            PreparedStatement insert = con.prepareStatement("INSERT INTO Driver VALUES (?,?,?)");
+            insert.setString(1,driverID);
+            insert.setString(2,dname);
+            insert.setInt(3,contactno);
+            insert.executeUpdate();
+            JOptionPane.showMessageDialog(null,"Record Added!");
+            con.close();  
+        }catch(Exception e){System.out.println(e);}
+        UpdateDriver bs = new UpdateDriver();
+        bs.setVisible(true);
+        bs.pack();
+        this.dispose();
+    }
+    
+   
+    
+
+    private void btnEditActionPerformed(ActionEvent evt) {
+        String driverID = textfid.getText();
+        int contactno = Integer.parseInt(textfcontact.getText());
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            Connection con=DriverManager.getConnection(  
+            "jdbc:mysql://localhost:3306/bus_system","root","*Laya2003*");  
+             
+            PreparedStatement insert = con.prepareStatement("UPDATE Driver set Contact = ? WHERE D_ID = ?;");
+            insert.setInt(1,contactno);
+            insert.setString(2,driverID);
             insert.executeUpdate();
             JOptionPane.showMessageDialog(null,"Record Updated!");
             con.close();  
@@ -215,8 +306,26 @@ public class UpdateDriver extends javax.swing.JFrame {
         bs.pack();
         this.dispose();
     }
- 
-   
+
+    private void btnDeleteActionPerformed(ActionEvent evt) {
+        String driverID = textfid.getText();
+
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            Connection con=DriverManager.getConnection(  
+            "jdbc:mysql://localhost:3306/bus_system","root","*Laya2003*");  
+             
+            PreparedStatement delete = con.prepareStatement("DELETE FROM Driver WHERE D_ID=?");
+            delete.setString(1,driverID);
+            delete.executeUpdate();
+            JOptionPane.showMessageDialog(null,"Record Deleted!");
+            con.close();  
+        }catch(Exception e){System.out.println(e);}
+        UpdateDriver bs = new UpdateDriver();
+        bs.setVisible(true);
+        bs.pack();
+        this.dispose();
+    }
 
 
      public static void main(String args[]) {
@@ -251,11 +360,12 @@ public class UpdateDriver extends javax.swing.JFrame {
     //private javax.swing.JLabel lblNow;
     //private javax.swing.JButton btnAttendance;
     private javax.swing.JButton btnBack;
-    private javax.swing.JTable studentdetails;
+    private javax.swing.JTable driverdetails;
     private javax.swing.JLabel idlabel;
     private javax.swing.JLabel namelabel;
     private javax.swing.JLabel contactlabel;
-    private javax.swing.JTextField textfUpdateDriver;
+    private javax.swing.JTextField textfcontact;
+    private javax.swing.JTextField textfname;
     private javax.swing.JTextField textfid;
     private javax.swing.JDesktopPane dp;
     private javax.swing.JPanel panelStatus;
